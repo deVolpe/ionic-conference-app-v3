0dc7abb906cd62b16d69c0accec446502fb8638d
Vladimir <successcraftlabtasks@gmail.com>
2019-10-09 23:53:55 +0300

    add order-form interfaces

---
+++ src/interfaces/order-form-options.ts
@@ -0,0 +1,14 @@
+export interface ReceiverOptions {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
+
+export interface ShippingOptions {
+  address: string;
+  city: string;
+  country: string;
+  state: string;
+  postalCode: string;
+}
