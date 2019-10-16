35b0e944ab96eca3c8e29b82354c114016f15f3f
Vladimir <successcraftlabtasks@gmail.com>
2019-10-16 14:53:59 +0300

    (order.module) :  add OrderFormComponent

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { OrderFormComponent } from './order-form/order-form';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from 'ionic-angular';
 
 @NgModule({
   declarations: [OrderFormComponent],
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(OrderFormComponent)
+  ],
   exports: [OrderFormComponent]
 })
 export class ComponentsModule {}
--- src/pages/order/order.module.ts
+++ src/pages/order/order.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { OrderPage } from './order';
+import { OrderFormComponent } from '../../components/order-form/order-form';
 
 @NgModule({
-  declarations: [OrderPage],
-  imports: [IonicPageModule.forChild(OrderPage)]
+  declarations: [OrderPage, OrderFormComponent],
+  imports: [IonicPageModule.forChild(OrderPage)],
+  exports: [OrderPage]
 })
 export class OrderPageModule {}
